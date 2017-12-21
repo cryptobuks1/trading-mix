5bdf8f82282fa4dcf60f0cd5c860b2f911b0105d
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2017-12-22 00:30:52 +0100

    Plotting prices

--- ohlc.py
+++ ohlc.py
@@ -1,11 +1,27 @@
 import json
+import matplotlib.dates as mdates
+import datetime
+import matplotlib.pyplot as plt
+
 def ohlcFile(path):
     with open(path) as fo:
         ohlcjsonstring=fo.read()
         ohlcjson=json.loads(ohlcjsonstring)
         for record in ohlcjson["result"]["XXMRZEUR"]:
+            record[0] = datetime.datetime.fromtimestamp(record[0])
             yield record
 
 if __name__ == "__main__":
-    for record in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json"):
-        print record
+    print [x[0] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")]
+    #plt.plot([x[1] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")])
+    # plt.plot_date(
+    #     mdates.date2num([x[0] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")]),
+    #     [x[1] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")]
+    # )
+    plt.plot(
+        [x[0] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")],
+        [y[1] for y in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")])
+    plt.gcf().autofmt_xdate()
+    plt.ylabel('Price in EURO')
+    plt.show()
+
