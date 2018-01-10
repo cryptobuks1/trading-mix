d9c0d1f01564c66a5c429c11bb3cc73b729a2448
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-10 07:40:24 +0100

    Format time

--- ohlc.py
+++ ohlc.py
@@ -3,25 +3,17 @@ import matplotlib.dates as mdates
 import datetime
 import matplotlib.pyplot as plt
 
-def ohlcFile(path):
+def readjsonfile(path):
     with open(path) as fo:
         ohlcjsonstring=fo.read()
         ohlcjson=json.loads(ohlcjsonstring)
         for record in ohlcjson["result"]["XXMRZEUR"]:
-            record[0] = datetime.datetime.fromtimestamp(record[0])
-            yield record
+            yield [float(pair[1]) if pair[0]==1 else pair[1] for pair in enumerate(record)]
 
 if __name__ == "__main__":
-    print [x[0] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")]
+    print [x[0] for x in readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")]
     #plt.plot([x[1] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")])
     # plt.plot_date(
     #     mdates.date2num([x[0] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")]),
     #     [x[1] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")]
     # )
-    plt.plot(
-        [x[0] for x in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")],
-        [y[1] for y in ohlcFile("/home/kristian/projects/kraken-bash/ohlc.json")])
-    plt.gcf().autofmt_xdate()
-    plt.ylabel('Price in EURO')
-    plt.show()
-
