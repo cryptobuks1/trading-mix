ed840b9365f7e0a51235fde10d208009cb1fa048
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-10 07:42:20 +0100

    Cleaning up

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ from matplotlib.dates import epoch2num
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-#record[0] = datetime.datetime.fromtimestamp(record[0])
 def plot():
     x = [x[0] for x in readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")]
     y = [y[1] for y in readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")]
-    print y
 
     z = np.polyfit(x, y, 2)
     f = np.poly1d(z)
@@ -18,11 +15,6 @@ def plot():
     y_new = f(x_new)
 
     plt.plot(x,y,'o', x_new, y_new)
-    # xs = readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")[:,0]
-    # ys = readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")[:,1]
-    # plt.plot(epoch2num(x), y)
-    # plt.plot(x, y)
-    # plt.gcf().autofmt_xdate()
     plt.ylabel('Price in EURO')
     plt.show()
 
