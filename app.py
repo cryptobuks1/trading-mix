895171f957b39b01a6344df40a095e423a8ce14c
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-12 08:20:50 +0100

    Find peaks

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from matplotlib.dates import epoch2num
 import matplotlib.pyplot as plt
 import numpy as np
+import peakutils
+
 
 def plot():
     x = [x[0] for x in readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")]
@@ -13,8 +15,9 @@ def plot():
 
     x_new = np.linspace(x[0], x[-1], 50)
     y_new = f(x_new)
+    peaks = peakutils.indexes(y_new, thres=0.5, min_dist=30)
 
-    plt.plot(x,y,'o', x_new, y_new)
+    plt.plot(x,y,'o', x_new, y_new,x_new[peaks],y_new[peaks],'+')
     plt.ylabel('Price in EURO')
     plt.show()
 
