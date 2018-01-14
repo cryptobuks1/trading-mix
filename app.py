e910eb429d0600c86c2701e2eb433c3c23ecd47c
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-14 11:25:22 +0100

    Read data once

--- app.py
+++ app.py
@@ -7,16 +7,22 @@ import peakutils
 
 
 def plot():
-    x = [x[0] for x in readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")]
-    y = [y[1] for y in readjsonfile("/home/kristian/projects/kraken-bash/ohlc.json")]
+    datafile = "/home/kristian/projects/kraken-bash/ohlc-1513269300.json"
+    datafile = "/home/kristian/projects/kraken-bash/ohlc.json"
+    ohlcdata = [record for record in readjsonfile(datafile)]
+    x = [record[0] for record in ohlcdata]
+    y = [record[1] for record in ohlcdata]
 
     z = np.polyfit(x, y, 2)
     f = np.poly1d(z)
 
+
     x_new = np.linspace(x[0], x[-1], 50)
     y_new = f(x_new)
     peaks = peakutils.indexes(y_new, thres=0.5, min_dist=30)
-    trade = np.array([yt + 1 for yt in peaks])
+    print datetime.fromtimestamp(x[0])
+    print datetime.fromtimestamp(x[peaks[0]])
+    trade = np.array([yt + 10 for yt in peaks])
 
     plt.plot(x,y,'o',
              x_new, y_new,
