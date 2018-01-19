88eff06fd6618a2a14d2996401e95b5a562f08c8
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-19 19:03:16 +0100

    Load all data

--- app.py
+++ app.py
@@ -15,27 +15,46 @@ def plot():
     print_start_end(ohlc_1513226220)
 
     ohlcdata = ohlc_1513226220
+    ohlcdata_sub = [record for record in ohlcdata if record[0] < 1513249991]
     x = [record[0] for record in ohlcdata]
     y = [record[1] for record in ohlcdata]
+    x_sub = [record[0] for record in ohlcdata_sub]
+    y_sub = [record[1] for record in ohlcdata_sub]
     x40 = x[0:360]
     y40 = y[0:360]
     data40 = [x40, y40]
 
     z = np.polyfit(x, y, 2)
     f = np.poly1d(z)
+    z_sub = np.polyfit(x_sub, y_sub, 2)
+    f_sub = np.poly1d(z_sub)
 
 
     x_new = np.linspace(x[0], x[-1], 50)
     y_new = f(x_new)
+    x_sub_new = np.linspace(x_sub[0], x_sub[-1], 50)
+    y_sub_new = f_sub(x_sub_new)
+
+
+
     peaks = peakutils.indexes(y_new, thres=0.5, min_dist=30)
+    peaks_sub = peakutils.indexes(y_sub_new, thres=0.5, min_dist=30)
     # print datetime.fromtimestamp(x[0])
-    print x_new[peaks[0]]
+    print peaks
+    print "peak"
+    print int(x_new[peaks[0]])
     print toDate(x_new[peaks[0]])
-    # trade = np.array([yt + 10 for yt in peaks])
+    trade = np.array([t + 10 for t in peaks])
+    print "trade"
+    print int(x_new[trade[0]])
+    print toDate(int(x_new[trade[0]]))
     plots = [x,y,'o']
     plots.extend(data40)
     plots.extend([x_new, y_new])
-    plots.extend([x_new[peaks], y_new[peaks], '+'])
+    plots.extend([x_sub_new, y_sub_new])
+    plots.extend([x_new[peaks], y_new[peaks], 'r+'])
+    plots.extend([x_sub_new[peaks_sub], y_sub_new[peaks_sub], '+'])
+    plots.extend([x_new[trade], y_new[trade], 'y+'])
 
     # plt.plot(
     #          x_new, y_new,
