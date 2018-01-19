671413f534f494278ba202fb77ef644d1bfb39e8
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-19 15:19:20 +0100

    Add utils

---
+++ util.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def print_start_end(record):
+    print record[0][0]
+    print datetime.fromtimestamp(record[0][0])
+    print record[-1][0]
+    print datetime.fromtimestamp(record[-1][0])
+
+def toDate(epoc):
+    return datetime.fromtimestamp(epoc)
