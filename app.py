0ce5181fc433067f951a36e6bc9e3b037a623717
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-22 16:49:16 +0100

    Change trade point

--- app.py
+++ app.py
@@ -17,14 +17,16 @@ def plot():
     print_start_end(ohlc_1513226220)
 
     ohlcdata = ohlc_1513226220
-    ohlcdata_sub = [record for record in ohlcdata if record[0] < 1513249991]
+    #1513226220 trade 10 1513249991
+    #1513226220 trade 5  1513245588
+    ohlcdata_sub = [record for record in ohlcdata if record[0] < 1513245588]
     xidx = 0
     yidx = 1
     [x, y] = xandy = extract(ohlcdata, xidx, yidx)
     [x_new, y_new, peaks] = fit(xandy)
     [x_sub_new, y_sub_new, peaks_sub] = fit(extract(ohlcdata, xidx, yidx))
     # print datetime.fromtimestamp(x[0])
-    trade = np.array([t + 10 for t in peaks])
+    trade = np.array([t + 5 for t in peaks])
     plots = [x,y,'o']
     plots.extend([x_new, y_new])
     plots.extend([x_new[peaks], y_new[peaks], 'r+'])
