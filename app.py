f93651acab1db86f3303ac8f9d051f9a763e39ff
Kristian Nygaard Jensen <kristian@debian-minifree.kringer.dk>
2018-01-22 09:02:39 +0100

    Use fit function

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import peakutils
 from util import print_start_end, toDate
+from data import fit, extract
 
 def plot():
     #datafile = "/home/kristian/projects/kraken-bash/ohlc-1513269300.json"
@@ -16,29 +17,11 @@ def plot():
 
     ohlcdata = ohlc_1513226220
     ohlcdata_sub = [record for record in ohlcdata if record[0] < 1513249991]
-    x = [record[0] for record in ohlcdata]
-    y = [record[1] for record in ohlcdata]
-    x_sub = [record[0] for record in ohlcdata_sub]
-    y_sub = [record[1] for record in ohlcdata_sub]
-    x40 = x[0:360]
-    y40 = y[0:360]
-    data40 = [x40, y40]
-
-    z = np.polyfit(x, y, 2)
-    f = np.poly1d(z)
-    z_sub = np.polyfit(x_sub, y_sub, 2)
-    f_sub = np.poly1d(z_sub)
-
-
-    x_new = np.linspace(x[0], x[-1], 50)
-    y_new = f(x_new)
-    x_sub_new = np.linspace(x_sub[0], x_sub[-1], 50)
-    y_sub_new = f_sub(x_sub_new)
-
-
-
-    peaks = peakutils.indexes(y_new, thres=0.5, min_dist=30)
-    peaks_sub = peakutils.indexes(y_sub_new, thres=0.5, min_dist=30)
+    xidx = 0
+    yidx = 1
+    [x, y] = xandy = extract(ohlcdata, xidx, yidx)
+    [x_new, y_new, peaks] = fit(xandy)
+    [x_sub_new, y_sub_new, peaks_sub] = fit(extract(ohlcdata, xidx, yidx))
     # print datetime.fromtimestamp(x[0])
     print peaks
     print "peak"
@@ -49,7 +32,6 @@ def plot():
     print int(x_new[trade[0]])
     print toDate(int(x_new[trade[0]]))
     plots = [x,y,'o']
-    plots.extend(data40)
     plots.extend([x_new, y_new])
     plots.extend([x_sub_new, y_sub_new])
     plots.extend([x_new[peaks], y_new[peaks], 'r+'])
