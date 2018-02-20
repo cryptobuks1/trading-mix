5bf3b521baf07dcf1e83f287e5bb396524cd0df2
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2018-02-20 08:44:14 +0100

    Represent json ohlc data as list of lists

--- lisp/json.lisp
+++ lisp/json.lisp
@@ -1,5 +1,12 @@
-(ql:quickload "yason")
-(with-open-file (stream "~/projects/kraken-bash/ohlc-1513226220.json")
-  (loop for object = (read-line stream nil nil)
-        while object
-        collect (yason:parse object)))
+(ql:quickload 'yason)
+(ql:quickload 'alexandria)
+(defun load-ohlc (query)
+  (with-open-file (stream "~/projects/kraken-bash/ohlc-1513226220.json")
+    (loop for object = (read-line stream nil nil)
+          while object
+          collect (funcall query (yason:parse object)))))
+
+(defun get-data (json)
+  (gethash (string "XXMRZEUR")(gethash (string "result") json)))
+
+;;; (first(first(load-ohlc #'get-data)))
