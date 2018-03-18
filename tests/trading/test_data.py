2b6616c52eb1f46f933cd3d3bc822fe000491181
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2018-03-18 23:16:03 +0100

    Add tests

--- tests/trading/test_data.py
+++ tests/trading/test_data.py
@@ -15,11 +15,14 @@ def test_fit():
     plt.show()
     assert True
 
+
+def ohlc_1513226220():
+    from trading.ohlc import readjsonfile
+    return [record for record in readjsonfile("/home/kristian/projects/kraken-bash/ohlc-1513226220.json")]
+
 @pytest.mark.range
 def test_range():
-    from trading.ohlc import readjsonfile
-    ohlc_1513226220 = [record for record in readjsonfile("/home/kristian/projects/kraken-bash/ohlc-1513226220.json")]
-    first_hour = extract(filter(lambda p: 1513226220 <= p[0] > (1513226220 + (3 * 3600)), ohlc_1513226220))
+    three_hours = extract(filter(lambda p: 1513226220 <= p[0] > (1513226220 + (3 * 3600)), ohlc_1513226220()))
 
     [x, y] = extract(ohlc_1513226220)
     assert(len(x) == len(y))
@@ -28,3 +31,26 @@ def test_range():
     plt.plot(x, y, subx, suby)
     plt.show()
     assert True
+
+
+def test_sub_fit():
+    fit(extract(filter(lambda p: 1513226220 <= p[0] > (1513226220 + (3 * 3600)), ohlc_1513226220())))
+
+
+def test_full_fit():
+    import numpy as np
+    #data = extract(ohlc_1513226220())
+    data = extract(filter(lambda p: 1513226220 <= p[0] > (1513226220 + (3 * 3600)), ohlc_1513226220()))
+    x, y, xfit, yfit = fit(data)
+    print peaks(yfit)
+
+
+def test_sliding_window():
+    data = ohlc_1513226220()
+    start = 1513226220
+    end = (start + (3 * 3600))
+    window = filter(lambda p: start <= p[0] > end, data)
+    x, y, xfit, yfit = fit(extract(window))
+    p = peaks(yfit)
+    if p:
+        plt.plot(x, y, xfit, yfit, xfit[p], yfit[p], '+')
