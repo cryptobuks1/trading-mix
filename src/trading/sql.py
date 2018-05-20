694d9c5d51a0dfbd6e42265d2227b0a44cbf8425
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-05-20 23:35:05 +0200

    Make sure range is int

--- src/trading/sql.py
+++ src/trading/sql.py
@@ -1,4 +1,6 @@
 def window(con, start, end):
+    start = int(start)
+    end = int(end)
     cur = con.cursor()
     cur.execute("SELECT timestamp, open FROM ohlc WHERE timestamp >= ? AND timestamp < ? ", (start, end))
     result = cur.fetchall()
