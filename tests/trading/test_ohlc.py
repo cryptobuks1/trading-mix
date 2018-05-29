314972bd46b9d2457cdd8f299874fd55a9535043
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-05-29 21:01:04 +0200

    Query latest ohlc from Kraken

---
+++ tests/trading/test_ohlc.py
@@ -0,0 +1,22 @@
+import krakenex
+from pykrakenapi import KrakenAPI
+import sqlite3
+
+
+def get_rate():
+    api = krakenex.API()
+    k = KrakenAPI(api)
+    #ohlc, last = k.get_ohlc_data("BCHUSD")
+    #ohlc, last = k.get_ohlc_data("XXMRZEUR")
+    return k.get_ohlc_data("XXMRZEUR")
+
+
+def memdb():
+    return sqlite3.connect(":memory:")
+
+
+def sql():
+    conn = memdb()
+    ohlc, last = get_rate()
+    ohlc.to_sql('ohlc', conn, index=False)
+    print(type(ohlc))
