871e3e138415a9e78f3a8d404fc0e8f3d49f778c
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-06-01 22:22:56 +0200

    Ohlc in kraken

--- src/trading/kraken.py
+++ src/trading/kraken.py
@@ -1,8 +1,20 @@
 import krakenex
 from pykrakenapi import KrakenAPI
+from trading.sql import memdb
 
 
 def get_rate():
     api = krakenex.API()
     k = KrakenAPI(api)
     return k.get_ohlc_data("XXMRZEUR")
+
+
+def ohlc():
+    conn, cur = memdb()
+    try:
+        ohlc, last = get_rate()
+    except Exception as e:
+        print("Exception")
+        print(e)
+    ohlc.to_sql('ohlc', conn, index=False)
+    return cur
