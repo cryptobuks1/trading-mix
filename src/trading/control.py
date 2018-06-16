acddc0659ac539fae5ed90cf42ac9779e57c7d7b
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-06-16 09:38:28 +0200

    status

---
+++ src/trading/control.py
@@ -0,0 +1,10 @@
+import pickle
+from os.path import join
+
+def handleError(e):
+    print("HHH")
+    print(e)
+
+
+def writeData(dataDir, data):
+    path = join(dataDir, )
---
+++ tests/trading/test_analyse.py
@@ -0,0 +1,13 @@
+from os.path import join
+from trading.sql import connect, time_range
+from trading.kraken import table_mapping, orders_table
+
+
+def test_yield_peak():
+    print(join('/home/kristian/projects/trading/data',
+    'alldata.sqlite'))
+    db = connect("sqlite:///" + join('/home/kristian/projects/trading/data',
+                                    'alldata.sqlite'))
+    env = {**db, **table_mapping[orders_table]}
+    start, end = time_range(**env)
+    print(start, end)
