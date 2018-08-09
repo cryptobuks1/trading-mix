847a277f97fa6354582c55681dde89f14f64a0f9
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-08-09 21:53:54 +0200

    take 4 peaks in profit test

--- tests/trading/helpers/strategy_helper.py
+++ tests/trading/helpers/strategy_helper.py
@@ -1,2 +1,18 @@
-def take(count, tradeCommands, **kwargs):
-    pass
+from trading.strategy.simple import create
+from eventhelpers import update_order_with_peak
+from trading.events import bind
+
+
+def take(count, tradeCommands, gen):
+    latest_order_epoc = {'epoc': 0}
+    speaks = []
+
+    engine, events = create(lambda: latest_order_epoc['epoc'],
+                            tradeCommands)
+    update_order_with_peak(events, latest_order_epoc)
+    bind(events.newPeak,
+         lambda peak_analysis: speaks.append(peak_analysis['xpeak'][0]))
+    for window in (w if len(speaks) < count
+                   else (_ for _ in ()).throw(StopIteration())
+                   for w in gen):
+        engine(window)
--- tests/trading/test_profit.py
+++ tests/trading/test_profit.py
@@ -1,6 +1,6 @@
 from trading.core import TradeCommand
+from trading.data import window_generator
 import pytest
-import logging
 
 
 from strategy_helper import take
@@ -11,6 +11,14 @@ tradeCommands = {
     TradeCommand.buy: lambda: True
 }
 
+
 @pytest.mark.profit
 def test_profit(all_data):
-    take(20, tradeCommands, **all_data)
+    euros = 1000
+    xmrs = 5
+    take(4,
+         tradeCommands,
+         window_generator(3600 * 3,
+                          600,
+                          **all_data))
+    assert xmrs > 5 or euros > 1000
