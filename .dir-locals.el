3e5d3aff577b99091ca3083e2f5f55ca4f75c9a2
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-08-09 21:27:54 +0200

    dir locals not needed

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-((python-mode . ((setq python-shell-interpreter "jupyter"
-                       python-shell-interpreter-args "console --simple-prompt"
-                       python-shell-prompt-detect-failure-warning nil)
-                 (add-to-list 'python-shell-completion-native-disabled-interpreters
-                              "jupyter")
-                 (setenv "DATA_DIR" (concat (file-name-as-directory (projectile-project-root)) "data")))))
