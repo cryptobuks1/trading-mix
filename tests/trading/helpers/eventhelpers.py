da223ef125bff3e37ed4b55403b3a74576983b76
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-09-18 16:27:31 +0200

    indent

--- tests/trading/helpers/eventhelpers.py
+++ tests/trading/helpers/eventhelpers.py
@@ -7,4 +7,4 @@ def update_order_with_peak(events, latest_order_epoc):
 
 
 def update_order_epoc(latest_order_epoc, peak_analysis):
-        latest_order_epoc['epoc'] = peak_analysis['x'][0]
+    latest_order_epoc['epoc'] = peak_analysis['x'][0]
