1188f5314d6893f6a77a2550b7febab11eef8ab8
Kristian Nygaard Jensen <kristian@freeduck.dk>
2018-12-16 20:43:27 +0100

    Cleaning up

--- poly-matrix.rkt
+++ poly-matrix.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require math plot)
 
-(provide poly fit)
+(provide poly fit fit-values)
 
 (define xs '(0 1  2  3  4  5   6   7   8   9  10))
 (define ys '(1 6 17 34 57 86 121 162 209 262 321))
@@ -11,6 +11,10 @@
   (define V (vandermonde-matrix x (+ n 1)))
   (define VT (matrix-transpose V))
   (matrix->vector (matrix-solve (matrix* VT V) (matrix* VT Y))))
+
+(define (fit-values xandy n)
+  (let-values ([(x y) xandy])
+    (fit x y n)))
 
 (define ((poly v) x)
   (for/sum ([c v] [i (in-naturals)])
--- visualize.rkt
+++ visualize.rkt
@@ -4,7 +4,19 @@
 (require db)
 (require "data.rkt")
 
-(define (2018-8-21-18-21)
+(define (s-curve-data start end)
+  (define *db*
+    (sqlite3-connect #:database
+                     "data/ohcl-2018-08-22-00:17:13.sqlite"))
+  (date-range *db*
+              start
+              end))
+
+(define (extract data)
+  (values (map (lambda (v)(vector-ref v 0)) data)
+          (map (lambda (v)(vector-ref v 1)) data)))
+
+(define (plot-top-of-s)
   (define *db*
     (sqlite3-connect #:database
                      "data/ohcl-2018-08-22-00:17:13.sqlite"))
@@ -15,3 +27,28 @@
   (define ys (map (lambda (v)(vector-ref v 1)) s-curve-data))
   (plot (list (points   (map vector xs ys))
               (function (poly (fit xs ys 2))))))
+
+(define (fit-top-of-s)
+  (define *db*
+    (sqlite3-connect #:database
+                     "data/ohcl-2018-08-22-00:17:13.sqlite"))
+  (define s-curve-data (date-range *db*
+                                   '(0 50 18 21 8 2018)
+                                   '(0 20 20 21 8 2018)))
+  (define xs (map (lambda (v)(vector-ref v 0)) s-curve-data))
+  (define ys (map (lambda (v)(vector-ref v 1)) s-curve-data))
+  (fit xs ys 2))
+
+
+(define (plot-full-s-curve-data)
+  (let*-values ([(data) (s-curve-data '(0 50 18 21 8 2018)
+                                      '(0 20 21 21 8 2018))]
+                [(x y) (extract data)])
+    (plot (list (points   (map vector x y))
+                (function (poly (fit x y 2)))))))
+
+(define (fit-full-s-curve-data)
+  (let*-values ([(data) (s-curve-data '(0 50 18 21 8 2018)
+                             '(0 20 21 21 8 2018))]
+                [(x y) (extract data)])
+    (fit x y 2)))
